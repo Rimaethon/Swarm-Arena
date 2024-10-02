b83ebd579c1dba72ad9bdddae314832e2eabfc6d
Yoldaş Ulaş <yutalas@gmail.com>
2024-10-02 15:16:18 +0300

    UIQualityButton and UISoundButton classes are added

---
+++ Assets/_Scripts/UI/Buttons/UIQualityButton.cs
@@ -0,0 +1,26 @@
+﻿using Rimaethon.Runtime.UI;
+using TMPro;
+using UnityEngine;
+
+namespace _Scripts.UI
+{
+	public class UIQualityButton:UIButton
+	{
+		[SerializeField] TextMeshProUGUI qualityText;
+		private string[] qualityNames;
+
+		protected override void Awake()
+		{
+			base.Awake();
+			qualityNames = QualitySettings.names;
+			qualityText.text = qualityNames[QualitySettings.GetQualityLevel()];
+		}
+
+		protected override void DoOnClick()
+		{
+			base.DoOnClick();
+			QualitySettings.SetQualityLevel((QualitySettings.GetQualityLevel() + 1) % qualityNames.Length);
+			qualityText.text = qualityNames[QualitySettings.GetQualityLevel()];
+		}
+	}
+}
---
+++ Assets/_Scripts/UI/Buttons/UISoundButton.cs
@@ -0,0 +1,57 @@
+﻿using Data;
+using Managers;
+using Rimaethon.Runtime.UI;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+	public class UISoundButton:UIButton
+	{
+		[SerializeField] private Sprite soundOnImage;
+		[SerializeField] private Sprite soundOffImage;
+		[SerializeField] private bool isSFX;
+		[SerializeField] private TextMeshProUGUI text;
+		[SerializeField] private Image image;
+		private SettingsData settingsData;
+
+		protected override void Awake()
+		{
+			base.Awake();
+			settingsData = SaveManager.Instance.GetSettingsData();
+			HandleVisual();
+		}
+
+		private void HandleVisual()
+		{
+			bool isOptionOn = isSFX ? settingsData.IsSFXOn : settingsData.IsMusicOn;
+
+			if (text != null)
+			{
+				text.text = isOptionOn ? "On" : "Off";
+			}
+
+			if (image != null)
+			{
+				image.sprite = isOptionOn ? soundOnImage : soundOffImage;
+			}
+		}
+
+		protected override void DoOnClick()
+		{
+			base.DoOnClick();
+			if(isSFX)
+			{
+				settingsData.IsSFXOn = !settingsData.IsSFXOn;
+				HandleVisual();
+			}
+			else
+			{
+				settingsData.IsMusicOn = !settingsData.IsMusicOn;
+				HandleVisual();
+			}
+			SaveManager.Instance.SetSettingsData(settingsData);
+		}
+	}
+}
