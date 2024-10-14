102a5d8a1dba7837df1be3084f5ff780f5afaae1
Yoldaş Ulaş <yutalas@gmail.com>
2024-10-14 18:00:40 +0300

    Enemy Detector class created, Player controller updated

---
+++ Assets/_Scripts/Player/EnemyDetector.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Interfaces;
+using UnityEngine;
+
+namespace Player
+{
+	public class EnemyDetector
+	{
+		public  float radius;
+		public int Size { get; private set; }
+		public IDamageAble[] damageAbles;
+		private  Collider[] results;
+		private readonly LayerMask enemyLayer;
+		private readonly Transform playerTransform;
+		private const int max_size = 320;
+		private int initialSize = 20;
+
+		public EnemyDetector(Transform playerTransform, LayerMask enemyLayer)
+		{
+			this.playerTransform = playerTransform;
+			this.enemyLayer = enemyLayer;
+			damageAbles = new IDamageAble[initialSize];
+			results = new Collider[initialSize];
+		}
+
+		public void GetDamageAblesInLargestRange()
+		{
+			Vector3 position = playerTransform.position;
+			Array.Clear(results, 0, results.Length);
+			Array.Clear(damageAbles,0,damageAbles.Length);
+			Size=Physics.OverlapSphereNonAlloc(position, radius/2,results, enemyLayer);
+			if(Size==0)
+				return;
+			while(Size>initialSize && initialSize<max_size)
+			{
+				initialSize*=2;
+				damageAbles=new IDamageAble[initialSize];
+				results=new Collider[initialSize];
+				Size=Physics.OverlapSphereNonAlloc(position, radius,results, enemyLayer);
+			}
+
+			int index = 0;
+			for(int i=0;i<Size;i++)
+			{
+				if (results[i] == null)
+					break;
+				if (!results[i].TryGetComponent(out IDamageAble damageAble)) continue;
+				damageAbles[index]=damageAble;
+				index++;
+			}
+			Size = index;
+			if (Size < 2)
+				return;
+			Array.Sort(damageAbles, 0, Size, new DamageAbleComparer(position));
+		}
+
+		private class DamageAbleComparer : IComparer<IDamageAble>
+		{
+			private readonly Vector3 position;
+
+			public DamageAbleComparer(Vector3 position)
+			{
+				this.position = position;
+			}
+			public int Compare(IDamageAble a, IDamageAble b)
+			{
+				if (a == null) return 1;
+				if (b == null) return -1;
+				return Vector3.Distance(a.Position, position).CompareTo(Vector3.Distance(b.Position, position));
+			}
+		}
+	}
+}
--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -1,147 +1,116 @@
-using System;
 using System.Collections.Generic;
-using _Scripts.Player.Weapons;
 using Data;
+using Enums;
+using Interfaces;
 using Managers;
-using Unity.Mathematics;
+using Scriptable_Objects;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
 
-
-[RequireComponent(typeof(CharacterController))]
-public class PlayerController : MonoBehaviour
+namespace Player
 {
-    public float HorizontalSpeed => transform.InverseTransformDirection(moveDirection).normalized.x;
-    public float VerticalSpeed => transform.InverseTransformDirection(moveDirection).normalized.z;
-
-    [SerializeField] private float movementSpeed;
-    [SerializeField] private float rotationSpeed;
-    [SerializeField] private float aimRotationSpeed;
-    [SerializeField] private LayerMask enemyLayer;
-    [SerializeField] Vector3 colliderSize= new Vector3(5, 5, 5);
-
-    public Vector3 moveDirection;
-    private CharacterController characterController;
-
-    [Header("Character Rig Settings")]
-    [SerializeField] private Rig aimRig;
-    [SerializeField] private Transform aimTarget;
-    [SerializeField] private float aimSmoothTime;
-    [SerializeField] private Transform weaponHolder;
-    [SerializeField] private WeaponDatabaseSO weaponDatabase;
-
-    private float aimSmoothVelocity;
-    private const float aim_rig_weight = 1f;
-    private bool hasTarget;
-    private RangeWeapon currentWeapon;
-    private List<GameObject> sideWeapons = new List<GameObject>();
-    private PlayerAnimationManager playerAnimationManager;
-    private PlayerStatusManager playerStatusManager;
-    private readonly Collider[] results = new Collider[20];
-    PlayerData playerData;
-
-    private void Awake()
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerController : MonoBehaviour
     {
-        playerAnimationManager = GetComponent<PlayerAnimationManager>();
-        playerStatusManager = GetComponent<PlayerStatusManager>();
-        characterController = GetComponent<CharacterController>();
-        playerData = SaveManager.Instance.GetPlayerData();
-        InitializeWeapons();
-        currentWeapon.Construct(playerAnimationManager);
-    }
+        public float HorizontalSpeed => transform.InverseTransformDirection(moveDirection).normalized.x;
+        public float VerticalSpeed => transform.InverseTransformDirection(moveDirection).normalized.z;
 
-    private void Update()
-    {
-        if (playerStatusManager.IsDead)
-            return;
-        HandleAimingMovement();
-        aimRig.weight = Mathf.SmoothDamp(aimRig.weight, aim_rig_weight, ref aimSmoothVelocity, aimSmoothTime);
-        if (hasTarget)
+        [SerializeField] private float movementSpeed;
+        [SerializeField] private float rotationSpeed;
+
+        public Vector3 moveDirection;
+        private CharacterController characterController;
+
+        [Header("Character Rig Settings")]
+        [SerializeField] private Rig aimRig;
+        [SerializeField] private Transform aimTarget;
+        [SerializeField] private float aimSmoothTime;
+        [SerializeField] private Transform weaponHolder;
+        [SerializeField] private WeaponDatabaseSO weaponDatabase;
+
+        private float aimSmoothVelocity;
+        private const float aim_rig_weight = 1f;
+        private bool hasTarget;
+        private readonly List<IWeapon> weapons = new List<IWeapon>();
+        private PlayerAnimationManager playerAnimationManager;
+        private PlayerHealth playerHealth;
+        private PlayerData playerData;
+        private EnemyDetector enemyDetector;
+        private float largestRange;
+
+        private void Awake()
         {
-            currentWeapon.PerformShot();
-            return;
+            playerAnimationManager = GetComponent<PlayerAnimationManager>();
+            playerHealth = GetComponent<PlayerHealth>();
+            characterController = GetComponent<CharacterController>();
+            playerData = SaveManager.Instance.GetPlayerData();
+            enemyDetector = new EnemyDetector(transform,LayerMask.GetMask("Enemy"));
+            InitializeWeapons();
+            enemyDetector.radius = largestRange;
         }
-        aimTarget.position = transform.position + transform.forward * 6+Vector3.up;
-        CalculateMovementRotation();
-    }
 
-    private void InitializeWeapons()
-    {
-        foreach (WeaponDataSO weapon in weaponDatabase.items.Values)
+        private void InitializeWeapons()
         {
-            if(!playerData.playerTalents[weapon.itemID].isUnlocked)
-                continue;
-            if (weapon.weaponType == WeaponType.HANDGUN)
+            foreach (WeaponDataSO weaponData in weaponDatabase.items.Values)
             {
-                currentWeapon=Instantiate(weapon.weaponPrefab, weaponHolder).GetComponent<RangeWeapon>();
-                currentWeapon.InitializeWeapon(weapon);
+                if(!playerData.playerTalents[weaponData.itemID].isUnlocked)
+                    continue;
+
+                IWeapon weapon = weaponData.weaponType == WeaponType.HANDGUN ?
+                                     Instantiate(weaponData.weaponPrefab, weaponHolder).GetComponent<IWeapon>() : Instantiate(weaponData.weaponPrefab, transform).GetComponent<IWeapon>();
+
+                weapon.InitializeWeapon(weaponData,enemyDetector,playerAnimationManager);
+                weapons.Add(weapon);
+                largestRange = Mathf.Max(largestRange, weapon.Range);
+            }
+        }
+
+        private void Update()
+        {
+            if (playerHealth.IsDead)
+                return;
+            enemyDetector.GetDamageAblesInLargestRange();
+            Aim();
+            characterController.Move(moveDirection * (movementSpeed* Time.deltaTime) + new Vector3(0, Physics.gravity.y / 2, 0));
+            if (!hasTarget) return;
+            HandleWeaponShooting();
+        }
+
+        private void HandleWeaponShooting()
+        {
+            foreach (IWeapon weapon in weapons)
+            {
+                weapon.TryGiveDamage();
+            }
+        }
+
+        private void Aim()
+        {
+            hasTarget = enemyDetector.damageAbles[0] != null;
+            Vector3 position = hasTarget ? enemyDetector.damageAbles[0].Position : transform.position;
+            aimRig.weight = Mathf.SmoothDamp(aimRig.weight, aim_rig_weight, ref aimSmoothVelocity, aimSmoothTime);
+            Vector3 lookDirection=moveDirection;
+            if (hasTarget)
+            {
+                aimTarget.position = position+ transform.forward * 6+Vector3.up;
+                lookDirection = position - transform.position;
+                lookDirection.y = 0;
             }
             else
             {
-                GameObject skillWeapon = Instantiate(weapon.weaponPrefab, transform);
-                skillWeapon.GetComponent<IWeapon>().InitializeWeapon(weapon);
-                sideWeapons.Add(skillWeapon);
+                aimTarget.position = transform.position + transform.forward * 6+Vector3.up;
             }
+            if(lookDirection==Vector3.zero)
+                return;
+
+            Quaternion rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
         }
-    }
 
-    private void CalculateMovementRotation()
-    {
-        if (moveDirection == Vector3.zero) return;
-        Quaternion movementRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, movementRotation, rotationSpeed * Time.deltaTime);
-    }
-
-    private void HandleAimingMovement()
-    {
-        Aim();
-        AimMovement();
-    }
-
-    private void Aim()
-    {
-        Vector3 position = GetNearestEnemy(transform.position, 6f);
-        if (hasTarget == false)
-            return;
-        aimTarget.position = position+ transform.forward * 6+Vector3.up;
-        Vector3 direction = position - transform.position;
-        direction.y = 0;
-        Quaternion aimRotation = Quaternion.LookRotation(direction, Vector3.up);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, aimRotation, aimRotationSpeed * Time.deltaTime);
-    }
-
-    private void AimMovement()
-    {
-        characterController.Move(moveDirection * (movementSpeed* Time.deltaTime) + new Vector3(0, Physics.gravity.y / 2, 0));
-    }
-
-    private Vector3 GetNearestEnemy(Vector3 position, float radius)
-    {
-        Array.Clear(results, 0, results.Length);
-        Physics.OverlapBoxNonAlloc(position, colliderSize,results, quaternion.identity, enemyLayer);
-        Transform nearestEnemy=null;
-        float nearestDistanceSqr = Mathf.Infinity;
-
-        foreach (Collider collider in results)
+        private void OnDrawGizmos()
         {
-            if (collider == null)
-                continue;
-            if(collider.gameObject.TryGetComponent<IDamageAble>(out IDamageAble damageable) && damageable.IsDead)
-                continue;
-            float distanceSqr = (collider.transform.position - position).sqrMagnitude;
-            if (!(distanceSqr < nearestDistanceSqr)) continue;
-            nearestDistanceSqr = distanceSqr;
-            nearestEnemy = collider.transform;
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, largestRange);
         }
-
-        if(nearestEnemy == null)
-        {
-            hasTarget = false;
-            return Vector3.zero;
-        }
-
-        hasTarget = true;
-        return nearestEnemy.position;
     }
-
 }
