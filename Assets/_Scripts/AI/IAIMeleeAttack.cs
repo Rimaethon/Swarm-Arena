3b9b2ae1149d8ad719a7aba3f25b55dbdc1babc6
Yoldaş Ulaş <yutalas@gmail.com>
2024-10-16 12:24:43 +0300

    MeleeDamageCollider is removed and its responsibilities are added to MeleeAICombat class

--- Assets/_Scripts/AI/IAIMeleeAttack.cs
+++
@@ -1,5 +0,0 @@
-﻿public interface IAIMeleeAttack
-{
-    public void PerformLightMeleeAttack(BaseAIAgent agent, int damage);
-    public void PerformHeavyMeleeAttack(BaseAIAgent agent, int damage);
-}
